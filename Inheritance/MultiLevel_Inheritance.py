11b541a4d9a1094723db7eac5a41295db422247c
pallavim1125 <pallavim1125@gmail.com>
2026-02-05 10:05:56 +0530

    Implemented Day 3 assigment task about Multiple and MultiLevel Inheritance

---
+++ Inheritance/MultiLevel_Inheritance.py
@@ -0,0 +1,42 @@
+# Parent Class
+class Product:
+    def __init__(self, product_name, price):
+        self.product_name = product_name
+        self.price = price
+
+    def display_product(self):
+        print("Product Name:", self.product_name)
+        print("Price:", self.price)
+
+
+# Child Class (Level 2)
+class ElectronicProduct(Product):
+    def __init__(self, product_name, price, brand, warranty):
+        super().__init__(product_name, price)
+        self.brand = brand
+        self.warranty = warranty
+
+    def display_electronic_product(self):
+        print("Brand:", self.brand)
+        print("Warranty:", self.warranty)
+
+
+# Grandchild Class (Level 3)
+class MobilePhone(ElectronicProduct):
+    def __init__(self, product_name, price, brand, warranty, ram, storage):
+        super().__init__(product_name, price, brand, warranty)
+        self.ram = ram
+        self.storage = storage
+
+    def display_mobile_details(self):
+        print("RAM:", self.ram)
+        print("Storage:", self.storage)
+
+
+# Creating Object
+mobile = MobilePhone("iPhone 15", 80000, "Apple", "1 Year", "8GB", "256GB")
+
+# Displaying All Details
+mobile.display_product()
+mobile.display_electronic_product()
+mobile.display_mobile_details()
